9bb0f2a3ab4d25d65dc91982e634f2b4d4c9387f
kiyo7 <kiyo324783@icloud.com>
2022-06-02 12:49:18 +0900

    doc fin

---
+++ components/Avatar.tsx
@@ -0,0 +1,99 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { supabase } from "../utils/supabase";
+
+interface Props {
+  url: string;
+  size: number;
+  onUpload: (path: string) => void;
+}
+
+const Avatar: React.FC<Props> = ({ url, size, onUpload }) => {
+  const [avatarUrl, setAvatarUrl] = useState("");
+  const [uploading, setUploading] = useState(false);
+
+  useEffect(() => {
+    if (url) downloadImage(url);
+  }, [url]);
+
+  const downloadImage = async (path: string) => {
+    try {
+      const { data, error } = await supabase.storage
+        .from("avatars")
+        .download(path);
+      if (error) {
+        throw error;
+      }
+      const url = URL.createObjectURL(data!);
+      setAvatarUrl(url);
+    } catch (error: any) {
+      console.log("Error downloading image: ", error.message);
+    }
+  };
+
+  const uploadAvatar = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      setUploading(true);
+
+      if (!event.target.files || event.target.files.length === 0) {
+        throw new Error("You must select an image to upload.");
+      }
+
+      const file = event.target.files[0];
+      const fileExt = file.name.split(".").pop();
+      const fileName = `${Math.random()}.${fileExt}`;
+      const filePath = `${fileName}`;
+
+      let { error: uploadError } = await supabase.storage
+        .from("avatars")
+        .upload(filePath, file);
+
+      if (uploadError) {
+        throw uploadError;
+      }
+
+      onUpload(filePath);
+    } catch (error: any) {
+      alert(error.message);
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  return (
+    <div>
+      {avatarUrl ? (
+        <Image
+          width={150}
+          height={150}
+          src={avatarUrl}
+          alt="Avatar"
+          className="avatar image"
+        />
+      ) : (
+        <div
+          className="avatar no-image"
+          style={{ height: size, width: size }}
+        />
+      )}
+      <div style={{ width: size }}>
+        <label className="button primary block" htmlFor="single">
+          {uploading ? "Uploading ..." : "Upload"}
+        </label>
+        <input
+          style={{
+            visibility: "hidden",
+            position: "absolute",
+          }}
+          type="file"
+          id="single"
+          accept="image/*"
+          onChange={uploadAvatar}
+          disabled={uploading}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Avatar;
